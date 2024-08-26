de1d42131d00f3b0d3373b1cce70608df9a40546
imran khan <143625800+imran-khan-rehan@users.noreply.github.com>
2024-08-26 00:26:01 -0700

    layout(navbar,sidebar) pages routing

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <script setup>
+import DefaultLayout from '@/layouts/DefaultLayout.vue';
 </script>
 
 <template>
-  
+  <DefaultLayout />
 </template>
-
-<style scoped>
-
-</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { useRoute } from 'vue-router'; 
+
+
+const route = useRoute();
+
+
+const isActiveLink = (link) => {
+  return route.path === link;
+};
+</script>
+
+<template>
+  <nav class="hidden md:block w-64 bg-blue-600 text-white flex-col p-4 space-y-4 rounded-tr-2xl">
+    <h1 class="block py-2 px-4 text-3xl font-bold text-white mb-8">YOLO</h1>
+
+    <!-- Router Links -->
+    <router-link
+      to="/"
+      class="block py-2 px-4 font-semibold rounded"
+      :class="isActiveLink('/') ? 'bg-white text-black' : 'hover:bg-blue-500 text-white'"
+    >
+      Home
+    </router-link>
+
+    <router-link
+      to="/ideas"
+      class="block py-2 px-4 font-semibold rounded"
+      :class="isActiveLink('/ideas') ? 'bg-white text-black' : 'hover:bg-blue-500 text-white'"
+    >
+      Ideas
+    </router-link>
+
+    <router-link
+      to="/recipies"
+      class="block py-2 px-4 font-semibold rounded"
+      :class="isActiveLink('/recipies') ? 'bg-white text-black' : 'hover:bg-blue-500 text-white'"
+    >
+      Recipies
+    </router-link>
+
+    <router-link
+      to="/view-orders"
+      class="block py-2 px-4 font-semibold rounded"
+      :class="isActiveLink('/view-orders') ? 'bg-white text-black' : 'hover:bg-blue-500 text-white'"
+    >
+      View Orders
+    </router-link>
+  </nav>
+</template>
