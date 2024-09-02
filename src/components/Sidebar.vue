d67ac3122de23f1ee1eeb73dc487825819c086fb
codebydaud <daudrizvi0@gmail.com>
2024-09-02 16:07:27 +0500

    Update

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,9 +1,7 @@
 <script setup>
-import { useRoute } from 'vue-router'; 
-
+import { useRoute } from "vue-router";
 
 const route = useRoute();
-
 
 const isActiveLink = (link) => {
   return route.path === link;
@@ -11,14 +9,20 @@ const isActiveLink = (link) => {
 </script>
 
 <template>
-  <nav class="hidden md:block w-64 bg-blue-600 text-white flex-col p-4 space-y-4 rounded-tr-2xl">
+  <nav
+    class="hidden md:block w-64 bg-blue-600 text-white flex-col p-4 space-y-4 rounded-tr-2xl"
+  >
     <h1 class="block py-2 px-4 text-3xl font-bold text-white mb-8">YOLO</h1>
 
     <!-- Router Links -->
     <router-link
       to="/"
       class="block py-2 px-4 font-semibold rounded"
-      :class="isActiveLink('/') ? 'bg-white text-black' : 'hover:bg-blue-500 text-white'"
+      :class="
+        isActiveLink('/')
+          ? 'bg-white text-black'
+          : 'hover:bg-blue-500 text-white'
+      "
     >
       Home
     </router-link>
@@ -26,30 +30,46 @@ const isActiveLink = (link) => {
     <router-link
       to="/ideas"
       class="block py-2 px-4 font-semibold rounded"
-      :class="isActiveLink('/ideas') ? 'bg-white text-black' : 'hover:bg-blue-500 text-white'"
+      :class="
+        isActiveLink('/ideas')
+          ? 'bg-white text-black'
+          : 'hover:bg-blue-500 text-white'
+      "
     >
       Ideas
     </router-link>
 
     <router-link
-      to="/recipies"
+      to="/recipes"
       class="block py-2 px-4 font-semibold rounded"
-      :class="isActiveLink('/recipies') ? 'bg-white text-black' : 'hover:bg-blue-500 text-white'"
+      :class="
+        isActiveLink('/recipes')
+          ? 'bg-white text-black'
+          : 'hover:bg-blue-500 text-white'
+      "
     >
-      Recipies
+      Recipes
     </router-link>
 
     <router-link
       to="/orders"
       class="block py-2 px-4 font-semibold rounded"
-      :class="isActiveLink('/orders') ? 'bg-white text-black' : 'hover:bg-blue-500 text-white'"
+      :class="
+        isActiveLink('/orders')
+          ? 'bg-white text-black'
+          : 'hover:bg-blue-500 text-white'
+      "
     >
       View Orders
     </router-link>
     <router-link
       to="/generateRecipe"
       class="block py-2 px-4 font-semibold rounded"
-      :class="isActiveLink('/generateRecipe') ? 'bg-white text-black' : 'hover:bg-blue-500 text-white'"
+      :class="
+        isActiveLink('/generateRecipe')
+          ? 'bg-white text-black'
+          : 'hover:bg-blue-500 text-white'
+      "
     >
       Generate Recipies
     </router-link>
--- src/router/index.js
+++ src/router/index.js
@@ -2,19 +2,28 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import ViewIdeas from '@/views/ViewIdeas.vue';
 import ViewOrders from '@/views/ViewOrders.vue';
-import ViewRecipies from '@/views/ViewRecipies.vue';
+import ViewRecipesList from "@/views/ViewRecipesList.vue";
+import ViewRecipeDetails from "@/views/ViewRecipeDetails.vue";
 import UserProfile from '@/views/UserProfile.vue';
-import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import GenerateRecipe from '@/views/GenerateRecipe.vue';
 const routes = [
-  { path: '/', name: 'Home', component: Home },
-  { path: '/ideas', name: 'GenerateIdea', component: ViewIdeas },
-  { path: '/recipies', name: 'ViewIdeas', component: ViewRecipies },
-  
-  { path: '/generateRecipe/:id', name: 'GenerateRecipe', component: GenerateRecipe },
-  { path: '/orders', name: 'ViewOrders', component: ViewOrders },
+  { path: "/", name: "Home", component: Home },
+  { path: "/ideas", name: "GenerateIdea", component: ViewIdeas },
+  { path: "/recipes", name: "ViewRecipesList", component: ViewRecipesList },
+  {
+    path: "/recipe-details/:recipeId",
+    name: "ViewRecipeDetails",
+    component: ViewRecipeDetails,
+  },
 
-  { path: '/userprofile', name: 'UserProfile', component: UserProfile },
+  {
+    path: "/generateRecipe/:id",
+    name: "GenerateRecipe",
+    component: GenerateRecipe,
+  },
+  { path: "/orders", name: "ViewOrders", component: ViewOrders },
+
+  { path: "/userprofile", name: "UserProfile", component: UserProfile },
   // { path: '/defaultlayout', name: 'DefaultLayout', component: DefaultLayout },
 ];  
 
